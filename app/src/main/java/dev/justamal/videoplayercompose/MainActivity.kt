c06f7740da9234709629a2a1887ff6ddb7d95c83
JustAmalll <amalkrasavchik@gmail.com>
2022-10-22 15:30:53 +0300

    project done

--- app/src/main/java/dev/justamal/videoplayercompose/MainActivity.kt
+++ app/src/main/java/dev/justamal/videoplayercompose/MainActivity.kt
@@ -2,15 +2,113 @@ package dev.justamal.videoplayercompose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FileOpen
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.media3.ui.PlayerView
+import dagger.hilt.android.AndroidEntryPoint
 import dev.justamal.videoplayercompose.ui.theme.VideoPlayerComposeTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             VideoPlayerComposeTheme {
+                val viewModel = hiltViewModel<MainViewModel>()
+                val videoItems by viewModel.videoItems.collectAsState()
 
+                val selectVideoLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.GetContent(),
+                    onResult = { uri ->
+                        uri?.let(viewModel::addVideoUri)
+                    }
+                )
+
+                var lifecycle by remember {
+                    mutableStateOf(Lifecycle.Event.ON_CREATE)
+                }
+
+                val lifecycleOwner = LocalLifecycleOwner.current
+
+                DisposableEffect(key1 = lifecycleOwner) {
+                    val observer = LifecycleEventObserver { _, event ->
+                        lifecycle = event
+                    }
+                    lifecycleOwner.lifecycle.addObserver(observer)
+                    onDispose {
+                        lifecycleOwner.lifecycle.removeObserver(observer)
+                    }
+                }
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    AndroidView(
+                        factory = { context ->
+                            PlayerView(context).also {
+                                it.player = viewModel.player
+                            }
+                        },
+                        update = {
+                            when (lifecycle) {
+                                Lifecycle.Event.ON_PAUSE -> {
+                                    it.onPause()
+                                    it.player?.pause()
+                                }
+                                Lifecycle.Event.ON_RESUME -> {
+                                    it.onResume()
+                                }
+                                else -> Unit
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(16 / 9f)
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    IconButton(
+                        onClick = {
+                            selectVideoLauncher.launch("video/mp4")
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.FileOpen,
+                            contentDescription = "Select Video"
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    LazyColumn(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        items(videoItems) { item ->
+                            Text(text = item.name, modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable { viewModel.playVideo(item.contentUri) }
+                                .padding(16.dp)
+                            )
+                        }
+                    }
+                }
             }
         }
     }
---
+++ app/src/main/java/dev/justamal/videoplayercompose/VideoPlayerApp.kt
@@ -0,0 +1,7 @@
+package dev.justamal.videoplayercompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class VideoPlayerApp : Application()
